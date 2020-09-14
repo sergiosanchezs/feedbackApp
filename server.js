3b8f23edf2f800af12c22c3337ef65b66a0f3e34
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-09-14 12:48:09 -0400

    speaker-detail page dynamic

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
 
 const FeedbackService = require('./services/FeedbackService');
 const SpeakersService = require('./services/SpeakerService');
@@ -15,10 +16,32 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['ASDASDASDaiudshas194', 'aisduh2398uhdiesdasASD'],
+  })
+);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
+app.locals.siteName = 'ROUX Meetups';
+
 app.use(express.static(path.join(__dirname, './static')));
+
+app.use(async (req, res, next) => {
+  try {
+    const names = await speakersService.getNames();
+    res.locals.speakersNames = names;
+    // console.log(res.locals);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
 
 app.use(
   '/',
